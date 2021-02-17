27c8af38f027b384e8ef49a94658bde670bcf55d
Vignesh Ganesan <20452299+viggggnesh@users.noreply.github.com>
2021-02-17 09:37:41 +0530

    Update mainComponent.R

--- Pokemon/mainComponent.R
+++ Pokemon/mainComponent.R
@@ -9,24 +9,23 @@ library(plotrix)
 library(compare)
 
 #opening dataset and printing summary of dataset
-pokemon_data <<- read.csv("C:/Users/gvign/Desktop/pokemon-indexer/pokemon/pokemon_stats.csv", stringsAsFactors = TRUE)
-summary(pokemon_data)
+pokemonData <<- read.csv("C:/Users/gvign/Desktop/pokemon-indexer/pokemon/pokemon_stats.csv", stringsAsFactors = TRUE)
 
 #starter pokemon
-starters <<- list(c("Bulbasaur", "Charmander", "Squirtle", "Chikorita", "Cyndaquil", "Totodile", "Treecko", "Torchic", "Mudkip"
+starterPokemon <<- list(c("Bulbasaur", "Charmander", "Squirtle", "Chikorita", "Cyndaquil", "Totodile", "Treecko", "Torchic", "Mudkip"
                    , "Turtwig", "Chimchar", "Piplup", "Snivy", "Tepig", "Oshawott", "Chespin", "Fennekin", "Froakie", "Rowlet",
                    "Litten", "Popolio"))
-starters <<- as.data.frame(starters, col.names = "Name")
-starters <<- match_df(pokemon_data, starters, on = "Name")
+starterPokemon <<- as.data.frame(starterPokemon, col.names = "Name")
+starterPokemon <<- match_df(pokemonData, starterPokemon, on = "Name")
 
 #splitting main data of Pokemon into sets based on their evolution to better perform operations on them. Each Pokemon has
 #a base form and an evolved form or even 2 evolved forms. Certain Pokemon also have Mega Evolution forms based on X or Y
 #evolution  stone.
-gen_all_normal <<- subset(pokemon_data, pokemon_data$Legendary == "FALSE" & !grepl("Mega ", pokemon_data$Name, fixed = TRUE))
-gen_all_legend <<- subset(pokemon_data, pokemon_data$Legendary == "TRUE" & !grepl("Mega ", pokemon_data$Name, fixed = TRUE))
-gen_all_mega <<- subset(pokemon_data, grepl("Mega ", pokemon_data$Name, fixed = TRUE))
+all_normal <<- subset(pokemonData, pokemonData$Legendary == "FALSE" & !grepl("Mega ", pokemonData$Name, fixed = TRUE))
+all_legend <<- subset(pokemonData, pokemonData$Legendary == "TRUE" & !grepl("Mega ", pokemonData$Name, fixed = TRUE))
+all_mega <<- subset(pokemonData, grepl("Mega ", pokemonData$Name, fixed = TRUE))
 
-#splitting based on generations 1 thorugh 7
+#splitting based on generations 1 through 7
 # gen1 <- subset(pokemon_data, pokemon_data$Generation=="1")
 # gen2 <- subset(pokemon_data, pokemon_data$Generation=="2")
 # gen3 <- subset(pokemon_data, pokemon_data$Generation=="3")
@@ -38,22 +37,22 @@ gen_all_mega <<- subset(pokemon_data, grepl("Mega ", pokemon_data$Name, fixed = 
 
 #plotting basic comparisons of pokemon stats
 #stat comparison of starter pokemon
-ggplot(starters, aes(x = reorder(Name,Number), y=Total, fill = Type1)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggplot(starterPokemon, aes(x = reorder(Name,Number), y=Total, fill = Type1)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 #distribution of pokemon by type
-type1_poke <- ggplot(data = gen_all_normal, aes(x = Type1, fill = Type1)) + geom_bar() + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + ggtitle("Distribution Based on Type-1") + coord_flip()
-type2_poke <- ggplot(data = gen_all_normal, aes(x = Type2, fill = Type2)) + geom_bar() + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + ggtitle("Distribution Based on Type-2") + coord_flip()
+type1_poke <- ggplot(data = all_normal, aes(x = Type1, fill = Type1)) + geom_bar() + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + ggtitle("Distribution Based on Type-1") + coord_flip()
+type2_poke <- ggplot(data = all_normal, aes(x = Type2, fill = Type2)) + geom_bar() + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + ggtitle("Distribution Based on Type-2") + coord_flip()
 grid.arrange(type1_poke, type2_poke, ncol = 2)
 
 #single vs dual-type pokemon
-pie3D(c(sum(!is.na(pokemon_data$Type1)) ,sum(pokemon_data$Type2 != "None")), labels = c("Type 1", "Type 1 and 2"), explode = 0.1, main = "Pokemon of Single and Dual Type nature")
+pie3D(c(sum(!is.na(pokemonData$Type1)) ,sum(pokemonData$Type2 != "None")), labels = c("Type 1", "Type 1 and 2"), explode = 0.1, main = "Pokemon of Single and Dual Type nature")
 
 
 #comparison of highest evolution vs mega evolution vs a legendary of the same type.
 
 
 #basic radar plot of pokemon
-radar_plot <- as.data.frame(starters[0:2, 6:11], ncol=  6)
+radar_plot <- as.data.frame(starterPokemon[0:2, 6:11], ncol=  6)
 
 colnames(radar_plot) <- c("HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed")
 rownames(radar_plot) <- c("Bulabasaur", "Charmander")
